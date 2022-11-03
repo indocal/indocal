ed8f6579e61c0ea05a9299d03dc5b5894d76b095
Diego Cruz <dcruz@indocal.gob.do>
2022-11-03 00:07:14 -0400

    fix: now <ErrorBoundary /> inherits <Widget /> height

--- packages/ui/layout/Widget/index.tsx
+++ packages/ui/layout/Widget/index.tsx
@@ -12,21 +12,17 @@ export interface WidgetProps extends BoxProps {
 export const Widget: React.FC<React.PropsWithChildren<WidgetProps>> = ({
   errorBoundaryMessage = 'Ha ocurrido un error al cargar este widget',
   disableDefaultSizes,
+  height,
   children,
   ...rest
 }) => (
-  <ErrorBoundary message={errorBoundaryMessage}>
-    <Box
-      height={
-        disableDefaultSizes || rest.height ? rest.height : WIDGET_SIZES.HEIGHT
-      }
-      {...rest}
-    >
-      {children}
-    </Box>
-  </ErrorBoundary>
+  <Box
+    height={disableDefaultSizes || height ? height : WIDGET_SIZES.HEIGHT}
+    {...rest}
+  >
+    <ErrorBoundary message={errorBoundaryMessage}>{children}</ErrorBoundary>
+  </Box>
 );
-
 export default Widget;
 
 ////////////////
