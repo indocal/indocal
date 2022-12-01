cdef5d4fcc5f022e227d1ff7b02eeb1239c1df8d
Diego Cruz <dcruz@indocal.gob.do>
2022-12-01 07:57:03 -0400

    refactor: use server-side fetching instead client-side fetching
    User server-side fetching instead client side fetching to prevent form data revalidation

--- apps/studio/pages/forms/preview/[form_id].tsx
+++ apps/studio/pages/forms/preview/[form_id].tsx
@@ -1,16 +1,21 @@
 import { useCallback } from 'react';
 import { GetServerSideProps } from 'next';
 import { useSession } from 'next-auth/react';
+import { getToken } from 'next-auth/jwt';
 import { Container } from '@mui/material';
 import { useSnackbar } from 'notistack';
 
-import { Form } from '@indocal/services';
+import { UUID, Form, INDOCAL } from '@indocal/services';
 import { Page } from '@indocal/ui';
 
 import { indocal } from '@/lib';
 import { FormGenerator, FormFieldAnswer } from '@/features';
 import { AdminDashboard } from '@/components';
 import { EnhancedNextPage } from '@/types';
+
+type FormPreviewPageParams = {
+  form_id: UUID;
+};
 
 type FormPreviewPageProps = {
   form: Form;
@@ -61,9 +66,31 @@ const FormPreviewPage: EnhancedNextPage<FormPreviewPageProps> = ({ form }) => {
 
 FormPreviewPage.getLayout = (page) => <AdminDashboard>{page}</AdminDashboard>;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps<
+  FormPreviewPageProps,
+  FormPreviewPageParams
+> = async (ctx) => {
+  const token = await getToken(ctx);
+
+  const indocal = new INDOCAL({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+    token: token?.access_token,
+  });
+
+  const { form } = await indocal.forms.findOneByUUID(
+    ctx.params?.form_id as string
+  );
+
+  if (!form) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
-    notFound: true,
+    props: {
+      form,
+    },
   };
 };
 
