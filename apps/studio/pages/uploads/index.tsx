bab39a02c91849876061f195cfe2700ff72f3f4a
Diego Cruz <dactjs@icloud.com>
2023-02-26 21:28:05 -0400

    add <LastFoldersGallery /> and <LastFilesGallery />

--- apps/studio/pages/uploads/index.tsx
+++ apps/studio/pages/uploads/index.tsx
@@ -1,39 +1,30 @@
 import { Container, Grid, Divider } from '@mui/material';
 
 import { Page } from '@indocal/ui';
-import { useFolders, useFiles } from '@indocal/services';
 
-import { FoldersGallery, FilesGallery } from '@/features';
+import { LastFoldersGallery, LastFilesGallery } from '@/features';
 import { AdminDashboard } from '@/components';
 import { EnhancedNextPage } from '@/types';
 
-const UploadsPage: EnhancedNextPage = () => {
-  const { folders } = useFolders();
-  const { files } = useFiles();
-
-  return (
-    <Page title="Librería de archivos" transition="down">
-      <Container fixed sx={{ paddingY: (theme) => theme.spacing(2) }}>
-        <Grid container justifyContent="center" alignItems="center" spacing={2}>
-          <Grid item xs={12}>
-            <FoldersGallery
-              title={`Carpetas (${folders.length})`}
-              folders={folders}
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Divider flexItem />
-          </Grid>
-
-          <Grid item xs={12}>
-            <FilesGallery title={`Archivos (${files.length})`} files={files} />
-          </Grid>
+const UploadsPage: EnhancedNextPage = () => (
+  <Page title="Librería de archivos" transition="down">
+    <Container fixed sx={{ paddingY: (theme) => theme.spacing(2) }}>
+      <Grid container justifyContent="center" alignItems="center" spacing={2}>
+        <Grid item xs={12}>
+          <LastFoldersGallery />
         </Grid>
-      </Container>
-    </Page>
-  );
-};
+
+        <Grid item xs={12}>
+          <Divider flexItem />
+        </Grid>
+
+        <Grid item xs={12}>
+          <LastFilesGallery />
+        </Grid>
+      </Grid>
+    </Container>
+  </Page>
+);
 
 UploadsPage.getLayout = (page) => <AdminDashboard>{page}</AdminDashboard>;
 
