71e5349aa906ac75ac7d48de9943ca7f593cc557
Diego Cruz <dcruz@indocal.gob.do>
2023-03-06 15:34:56 -0400

    feat: add minFiles and maxFiles rule to FilesFormFieldConfig

--- apps/studio/features/forms/components/FormFieldsCard/components/EditFormFieldDialog/components/FilesFormFieldConfig/index.tsx
+++ apps/studio/features/forms/components/FormFieldsCard/components/EditFormFieldDialog/components/FilesFormFieldConfig/index.tsx
@@ -1,31 +1,115 @@
-import { Stack } from '@mui/material';
+import { useMemo } from 'react';
+import { Stack, Divider, TextField, MenuItem } from '@mui/material';
 import { useFormContext, Control } from 'react-hook-form';
 
-import { ControlledCheckbox } from '@indocal/ui';
+import { ControlledCheckbox, ControlledSelect } from '@indocal/ui';
 
 import { EditFormFieldDialogData } from '../../context';
 
 export const FilesFormFieldConfig: React.FC = () => {
   const {
-    formState: { isSubmitting },
+    formState: { isSubmitting, errors },
+    register,
     control,
   } = useFormContext<EditFormFieldDialogData>();
 
-  return (
-    <Stack direction="row" justifyContent="space-between" spacing={2}>
-      <ControlledCheckbox
-        name="config.required"
-        label="¿Campo requerido?"
-        control={control as unknown as Control}
-        formControlProps={{ disabled: isSubmitting }}
-      />
+  const labels = useMemo(
+    () => ({
+      image: 'Imágenes',
+      video: 'Videos',
+      audio: 'Audios',
+      files: 'Archivos',
+    }),
+    []
+  );
 
-      <ControlledCheckbox
-        name="config.multiple"
-        label="¿Campo múltiple?"
+  return (
+    <Stack spacing={2} divider={<Divider flexItem />}>
+      <Stack direction="row" justifyContent="space-between" spacing={2}>
+        <ControlledCheckbox
+          name="config.required"
+          label="¿Campo requerido?"
+          control={control as unknown as Control}
+          formControlProps={{ disabled: isSubmitting }}
+        />
+
+        <ControlledCheckbox
+          name="config.multiple"
+          label="¿Campo múltiple?"
+          control={control as unknown as Control}
+          formControlProps={{ disabled: isSubmitting }}
+        />
+      </Stack>
+
+      <Stack direction="row" spacing={2}>
+        <TextField
+          fullWidth
+          size="small"
+          type="number"
+          autoComplete="off"
+          label="Archivos mínimos"
+          disabled={isSubmitting}
+          inputProps={register('config.minFiles', { valueAsNumber: true })}
+          error={Boolean(errors.config?.minFiles)}
+          helperText={errors.config?.minFiles?.message}
+        />
+
+        <TextField
+          fullWidth
+          size="small"
+          type="number"
+          autoComplete="off"
+          label="Archivos máximos"
+          disabled={isSubmitting}
+          inputProps={register('config.maxFiles', { valueAsNumber: true })}
+          error={Boolean(errors.config?.maxFiles)}
+          helperText={errors.config?.maxFiles?.message}
+        />
+      </Stack>
+
+      <ControlledSelect
+        name="config.accept"
+        label="Tipos de archivos aceptados"
         control={control as unknown as Control}
-        formControlProps={{ disabled: isSubmitting }}
-      />
+        formControlProps={{
+          fullWidth: true,
+          size: 'small',
+          required: true,
+          disabled: isSubmitting,
+        }}
+        selectProps={{
+          multiple: true,
+          renderValue: (selected) =>
+            Array.isArray(selected) &&
+            selected
+              .map((value: string) => labels[value as keyof typeof labels])
+              .sort()
+              .join(', '),
+        }}
+        controllerProps={{
+          defaultValue: [],
+          rules: {
+            required: {
+              value: true,
+              message: 'Debe completar este campo',
+            },
+          },
+        }}
+      >
+        <MenuItem value="image">
+          Imágenes (JPEG, PNG, GIF, SVG, TIFF, ICO, DVU)
+        </MenuItem>
+
+        <MenuItem value="video">
+          Videos (MPEG, MP4, Quicktime, WMV, AVI, FLV)
+        </MenuItem>
+
+        <MenuItem value="audio">Audios (MP3, WAV, OGG)</MenuItem>
+
+        <MenuItem value="files">
+          Archivos (Text, CSV, JSON, PDF, Word, Excel, PowerPoint)
+        </MenuItem>
+      </ControlledSelect>
     </Stack>
   );
 };
