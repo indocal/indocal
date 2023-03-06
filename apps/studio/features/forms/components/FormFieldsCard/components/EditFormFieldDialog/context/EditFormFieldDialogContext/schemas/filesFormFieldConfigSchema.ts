485cbc5e81fc0a545725d4b2fef64515ef44a73e
Diego Cruz <dcruz@indocal.gob.do>
2023-03-06 15:17:39 -0400

    feat: add minFiles and maxFiles rule to FilesFormFieldConfig

--- apps/studio/features/forms/components/FormFieldsCard/components/EditFormFieldDialog/context/EditFormFieldDialogContext/schemas/filesFormFieldConfigSchema.ts
+++ apps/studio/features/forms/components/FormFieldsCard/components/EditFormFieldDialog/context/EditFormFieldDialogContext/schemas/filesFormFieldConfigSchema.ts
@@ -1,11 +1,50 @@
 import { z as zod } from 'zod';
 
 export const filesFormFieldConfigSchema = zod.object({
+  accept: zod
+    .string({
+      description: 'Tipos de archivos aceptados',
+      required_error: 'Debe indicar los tipos de archivos aceptados',
+      invalid_type_error: 'Formato no válido',
+    })
+    .array()
+    .min(1, 'Debe indicar al menos un tipo de archivo aceptado'),
+
   multiple: zod.boolean({
     description: '¿Campo múltiple?',
     required_error: 'Debe indicar si el campo es múltiple o no',
     invalid_type_error: 'Formato no válido',
   }),
+
+  minFiles: zod
+    .number({
+      description: 'Archivos mínimos',
+      required_error: 'Debe indicar los archivos mínimos',
+      invalid_type_error: 'Formato no válido',
+    })
+    .or(
+      zod.nan({
+        description: 'Archivos mínimos',
+        required_error: 'Debe indicar los archivos mínimos',
+        invalid_type_error: 'Formato no válido',
+      })
+    )
+    .nullable(),
+
+  maxFiles: zod
+    .number({
+      description: 'Archivos máximos',
+      required_error: 'Debe indicar los archivos máximos',
+      invalid_type_error: 'Formato no válido',
+    })
+    .or(
+      zod.nan({
+        description: 'Archivos máximos',
+        required_error: 'Debe indicar los archivos máximos',
+        invalid_type_error: 'Formato no válido',
+      })
+    )
+    .nullable(),
 });
 
 export default filesFormFieldConfigSchema;
