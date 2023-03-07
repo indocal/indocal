be059064e64082639bfbaa7703598fa2ac077afc
Diego Cruz <dcruz@indocal.gob.do>
2023-03-07 11:10:16 -0400

    feat: add <FilesColumn /> to <TableFormField />

---
+++ packages/forms-generator/features/FormGenerator/components/TableFormField/components/FilesColumn/index.tsx
@@ -0,0 +1,106 @@
+import { useMemo } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import { ControlledFilesDropzone } from '@indocal/ui';
+import {
+  Form,
+  TableFormFieldColumn,
+  FilesFormFieldConfig,
+} from '@indocal/services';
+
+export interface FilesColumnProps {
+  field: Form['fields'][number];
+  column: TableFormFieldColumn;
+  row: number;
+}
+
+export const FilesColumn: React.FC<FilesColumnProps> = ({
+  field,
+  column,
+  row,
+}) => {
+  const {
+    formState: { isSubmitting },
+    control,
+  } = useFormContext();
+
+  const config = useMemo<FilesFormFieldConfig | null>(
+    () => column.config as FilesFormFieldConfig,
+    [column.config]
+  );
+
+  return (
+    <ControlledFilesDropzone
+      size="small"
+      required={config?.required}
+      multiple={config?.multiple}
+      name={`${field.id}.${row}.${column.heading}`}
+      control={control}
+      disabled={isSubmitting}
+      dropzoneProps={{
+        maxFiles: config?.maxFiles,
+        accept: {
+          ...(config?.accept?.includes('image') && {
+            'image/*': [
+              '.jpg',
+              '.jpeg',
+              '.png',
+              '.gif',
+              '.svg',
+              '.tiff',
+              '.ico',
+            ],
+          }),
+
+          ...(config?.accept?.includes('video') && {
+            'video/*': ['.mpeg', '.mp4', '.mov', '.wmv', '.avi', '.flv'],
+          }),
+
+          ...(config?.accept?.includes('audio') && {
+            'audio/*': ['.mp3', '.wav', '.ogg'],
+          }),
+
+          ...(config?.accept?.includes('files') && {
+            'text/*': ['.txt', '.csv'],
+            'application/*': [
+              '.json',
+              '.pdf',
+              '.doc',
+              '.docx',
+              '.xls',
+              '.xlsx',
+              '.ppt',
+              '.pptx',
+            ],
+          }),
+        },
+      }}
+      controllerProps={{
+        rules: {
+          required: {
+            value: Boolean(config?.required),
+            message: 'Debe completar este campo',
+          },
+
+          ...(config?.multiple && {
+            validate: {
+              ...(config?.minFiles && {
+                minFiles: (value) =>
+                  value.length >= config.minFiles ||
+                  `Debe ingresar al menos ${config.minFiles} archivos`,
+              }),
+
+              ...(config?.maxFiles && {
+                maxFiles: (value) =>
+                  value.length <= config.maxFiles ||
+                  `Debe ingresar como máximo ${config.maxFiles} archivos`,
+              }),
+            },
+          }),
+        },
+      }}
+    />
+  );
+};
+
+export default FilesColumn;
