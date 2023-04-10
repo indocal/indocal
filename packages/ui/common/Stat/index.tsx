fa942ac4269bbdfcac1a0996bcea5cdd953aa8a0
Diego Cruz <dactjs@icloud.com>
2023-04-10 00:20:02 -0400

    feat: complete trending feature on <Stat /> component

--- packages/ui/common/Stat/index.tsx
+++ packages/ui/common/Stat/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useMemo } from 'react';
 import {
   Stack,
   Card,
@@ -6,6 +7,7 @@ import {
   CardActions,
   ToggleButtonGroup,
   ToggleButton,
+  Typography,
   Tooltip,
   CircularProgress,
   LinearProgress,
@@ -22,6 +24,8 @@ import {
 export type StatTrending = {
   trend: 'UP' | 'FLAT' | 'DOWN';
   description: string;
+  currentValue: number;
+  previousValue: number;
 };
 
 export interface StatProps {
@@ -40,92 +44,149 @@ export const Stat: React.FC<StatProps> = ({
   trending,
   loading,
   validating,
-}) => (
-  <Card
-    sx={{
-      position: 'relative',
-      display: 'grid',
-      width: 250,
-      aspectRatio: '2 / 1',
-      overflow: 'hidden',
-    }}
-  >
-    {loading ? (
-      <CircularProgress sx={{ margin: 'auto' }} />
-    ) : (
-      <>
-        {validating && (
-          <LinearProgress
-            sx={{ position: 'absolute', top: 0, left: 0, width: '100%' }}
+}) => {
+  const [trendType, setTrendType] = useState<'#' | '%'>('%');
+
+  const trendValue = useMemo(() => {
+    if (!trending) return null;
+
+    const { currentValue, previousValue } = trending;
+
+    if (trendType === '#') {
+      return currentValue - previousValue;
+    }
+
+    return ((currentValue - previousValue) / previousValue) * 100;
+  }, [trending, trendType]);
+
+  return (
+    <Card
+      sx={{
+        position: 'relative',
+        display: 'grid',
+        width: 250,
+        aspectRatio: '16 / 9',
+        overflow: 'hidden',
+      }}
+    >
+      {loading ? (
+        <CircularProgress sx={{ margin: 'auto' }} />
+      ) : (
+        <>
+          {validating && (
+            <LinearProgress
+              sx={{ position: 'absolute', top: 0, left: 0, width: '100%' }}
+            />
+          )}
+
+          <CardHeader
+            title={title}
+            subheader={value}
+            titleTypographyProps={{
+              sx: {
+                display: '-webkit-box',
+                WebkitBoxOrient: 'vertical',
+                WebkitLineClamp: 1,
+                lineClamp: 1,
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+                wordBreak: 'break-word',
+              },
+            }}
+            subheaderTypographyProps={{
+              sx: {
+                display: '-webkit-box',
+                WebkitBoxOrient: 'vertical',
+                WebkitLineClamp: 1,
+                lineClamp: 1,
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+                wordBreak: 'break-word',
+                fontSize: '1.30em',
+                fontWeight: 'bolder',
+              },
+            }}
+            action={
+              description && (
+                <Tooltip title={description}>
+                  <InfoIcon fontSize="small" color="info" />
+                </Tooltip>
+              )
+            }
+            sx={{ marginBottom: 'auto' }}
           />
-        )}
 
-        <CardHeader
-          title={title}
-          subheader={value}
-          subheaderTypographyProps={{
-            sx: {
-              fontSize: '1.30em',
-              fontWeight: 'bolder',
-            },
-          }}
-          action={
-            description && (
-              <Tooltip title={description}>
-                <InfoIcon fontSize="small" color="info" />
+          {trending && (
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="flex-end"
+              sx={{
+                borderTop: (theme) => `1px dashed ${theme.palette.divider}`,
+              }}
+            >
+              <Tooltip title={trending.description}>
+                <CardContent
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'flex-end',
+                    gap: (theme) => theme.spacing(1),
+                    padding: (theme) => theme.spacing(1.5),
+                  }}
+                >
+                  {trending.trend === 'UP' && (
+                    <TrendingUpIcon fontSize="small" color="success" />
+                  )}
+
+                  {trending.trend === 'FLAT' && (
+                    <TrendingFlatIcon fontSize="small" color="disabled" />
+                  )}
+
+                  {trending.trend === 'DOWN' && (
+                    <TrendingDownIcon fontSize="small" color="error" />
+                  )}
+
+                  <Typography
+                    sx={{
+                      display: '-webkit-box',
+                      WebkitBoxOrient: 'vertical',
+                      WebkitLineClamp: 1,
+                      lineClamp: 1,
+                      textOverflow: 'ellipsis',
+                      overflow: 'hidden',
+                      wordBreak: 'break-word',
+                      letterSpacing: 0.75,
+                    }}
+                  >
+                    {trendType === '#'
+                      ? trendValue?.toFixed(2)
+                      : trendValue?.toFixed(2) + '%'}
+                  </Typography>
+                </CardContent>
               </Tooltip>
-            )
-          }
-          sx={{ marginBottom: 'auto' }}
-        />
 
-        {trending && (
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="flex-end"
-            sx={{ borderTop: (theme) => `1px dashed ${theme.palette.divider}` }}
-          >
-            <Tooltip title={trending.description}>
-              <CardContent
-                sx={{
-                  display: 'flex',
-                  alignItems: 'flex-end',
-                  gap: (theme) => theme.spacing(1),
-                  padding: (theme) => theme.spacing(1.5),
-                }}
-              >
-                {trending.trend === 'UP' && (
-                  <TrendingUpIcon fontSize="small" color="success" />
-                )}
+              <CardActions>
+                <ToggleButtonGroup
+                  exclusive
+                  size="small"
+                  value={trendType}
+                  onChange={(_, type) => setTrendType(type as '#' | '%')}
+                >
+                  <ToggleButton value="#">
+                    <NumberIcon sx={{ fontSize: 15 }} />
+                  </ToggleButton>
 
-                {trending.trend === 'FLAT' && (
-                  <TrendingFlatIcon fontSize="small" color="disabled" />
-                )}
-
-                {trending.trend === 'DOWN' && (
-                  <TrendingDownIcon fontSize="small" color="error" />
-                )}
-              </CardContent>
-            </Tooltip>
-
-            {/* TODO: complete this feature */}
-            <CardActions>
-              <ToggleButtonGroup exclusive size="small">
-                <ToggleButton value="#">
-                  <NumberIcon sx={{ fontSize: 15 }} />
-                </ToggleButton>
-
-                <ToggleButton value="%">
-                  <PercentIcon sx={{ fontSize: 15 }} />
-                </ToggleButton>
-              </ToggleButtonGroup>
-            </CardActions>
-          </Stack>
-        )}
-      </>
-    )}
-  </Card>
-);
+                  <ToggleButton value="%">
+                    <PercentIcon sx={{ fontSize: 15 }} />
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </CardActions>
+            </Stack>
+          )}
+        </>
+      )}
+    </Card>
+  );
+};
 
 export default Stat;
