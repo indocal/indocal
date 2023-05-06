c5a63983f4dfd933bb54f377be667bc9da3dd6ad
Diego Cruz <dactjs@icloud.com>
2023-05-05 20:19:36 -0400

    feat: use React Flow to represent with graphis the Services Steps Flow

--- apps/studio/features/services/components/ServiceCard/components/ManageServiceProcessDialog/index.tsx
+++ apps/studio/features/services/components/ServiceCard/components/ManageServiceProcessDialog/index.tsx
@@ -1,26 +1,24 @@
-import { useState, useCallback } from 'react';
+import { useState, useMemo, useCallback, useRef } from 'react';
 import {
   Dialog,
   DialogTitle,
   DialogContent,
   DialogActions,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-  ListItemSecondaryAction,
   Typography,
   IconButton,
 } from '@mui/material';
-import {
-  AddCircle as AddIcon,
-  Settings as SettingsIcon,
-  AccountTree as StepIcon,
-} from '@mui/icons-material';
+import { AddCircle as AddIcon } from '@mui/icons-material';
 import { LoadingButton } from '@mui/lab';
+import dagre from 'dagre';
+import ReactFlow, { Background, Node, Edge, Position } from 'reactflow';
+import 'reactflow/dist/style.css';
 
 import { NoData } from '@indocal/ui';
 import { Can, Service } from '@indocal/services';
+
+const NODE_POSITION = { x: 0, y: 0 };
+const NODE_WIDTH = 200;
+const NODE_HEIGHT = 50;
 
 import { useServiceCard } from '../../context';
 import { AddServiceProcessStepDialog, EditServiceProcessStepDialog } from '../';
@@ -56,6 +54,81 @@ export const ManageServiceProcessDialog: React.FC<
     toggleManageServiceProcessDialog();
   }, [toggleManageServiceProcessDialog]);
 
+  const dagreGraphRef = useRef(new dagre.graphlib.Graph());
+  dagreGraphRef.current.setGraph({ rankdir: 'LR' });
+  dagreGraphRef.current.setDefaultEdgeLabel(() => ({}));
+
+  const { nodes, edges } = useMemo(() => {
+    const nodes = service.steps.map<Node>((step) => ({
+      id: step.id,
+      position: NODE_POSITION,
+      sourcePosition: Position.Right,
+      targetPosition: Position.Left,
+      data: { step, label: step.title },
+    }));
+
+    const edges = service.steps
+      .map<Edge[]>((step) => {
+        const edges: Edge[] = [];
+
+        if (step.nextStepOnApprove) {
+          edges.push({
+            id: `${step.id}-${step.nextStepOnApprove.id}`,
+            type: 'step',
+            source: step.id,
+            target: step.nextStepOnApprove.id,
+            label: 'Aprobado',
+            animated: true,
+            style: { stroke: 'green' },
+          });
+        }
+
+        if (step.nextStepOnReject) {
+          edges.push({
+            id: `${step.id}-${step.nextStepOnReject.id}`,
+            type: 'step',
+            source: step.id,
+            target: step.nextStepOnReject.id,
+            label: 'Rechazado',
+            animated: true,
+            style: { stroke: 'red' },
+          });
+        }
+
+        return edges;
+      })
+      .flat();
+
+    if (dagreGraphRef.current) {
+      nodes.forEach((node) => {
+        dagreGraphRef.current.setNode(node.id, {
+          width: NODE_WIDTH,
+          height: NODE_HEIGHT,
+        });
+      });
+
+      edges.forEach((edge) => {
+        dagreGraphRef.current.setEdge(edge.source, edge.target);
+      });
+
+      dagre.layout(dagreGraphRef.current);
+
+      nodes.forEach((node) => {
+        const nodeWithPosition = dagreGraphRef.current.node(node.id);
+
+        node.targetPosition = Position.Left;
+        node.sourcePosition = Position.Right;
+
+        node.position = {
+          x: nodeWithPosition.x - NODE_WIDTH / 2,
+          y: nodeWithPosition.y - NODE_HEIGHT / 2,
+        };
+      });
+    }
+
+    return { nodes, edges };
+  }, [service.steps]);
+
   return (
     <>
       {isAddServiceProcessStepDialogOpen && (
@@ -88,27 +161,16 @@ export const ManageServiceProcessDialog: React.FC<
           </Can>
         </DialogTitle>
 
-        <DialogContent dividers>
+        <DialogContent dividers sx={{ padding: (theme) => theme.spacing(1) }}>
           {service.steps.length > 0 ? (
-            <List disablePadding>
-              {service.steps.map((step) => (
-                <ListItem key={step.id} divider>
-                  <ListItemIcon>
-                    <StepIcon />
-                  </ListItemIcon>
-
-                  <ListItemText>{step.title}</ListItemText>
-
-                  <Can I="update" a="service">
-                    <ListItemSecondaryAction>
-                      <IconButton edge="end" onClick={() => handleEdit(step)}>
-                        <SettingsIcon />
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </Can>
-                </ListItem>
-              ))}
-            </List>
+            <ReactFlow
+              fitView
+              nodes={nodes}
+              edges={edges}
+              onNodeClick={(_, node) => handleEdit(node.data.step)}
+            >
+              <Background />
+            </ReactFlow>
           ) : (
             <NoData message="Pasos aun sin definir" />
           )}
