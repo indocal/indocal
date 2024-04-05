fceb33c4b42d0c10462e6bba5512de49fec26503
Diego Cruz <dactjs@icloud.com>
2024-04-04 21:49:50 -0400

    corrigiendo huevo

--- apps/app/lib/indocal/index.ts
+++ apps/app/lib/indocal/index.ts
@@ -2,7 +2,7 @@ import INDOCAL from '@indocal/services';
 
 export const indocal = new INDOCAL({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
-  token: process.env.INDOCAL_API_TOKEN,
+  token: process.env.NEXT_PUBLIC_INDOCAL_API_TOKEN,
 });
 
 export default indocal;
